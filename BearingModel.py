c8d47df8dbdb29460d18d8cedee3baaa93b8cd7d
Administrator <icefoz233@qq.com>
2025-08-19 18:03:15 +0800

    新增外圈故障控制参数，但仍然存在问题。

--- BearingModel.py
+++ BearingModel.py
@@ -2,11 +2,17 @@ import numpy as np
 from numpy import sin, cos, pi
 
 
-def ode_system(t, y, Fr, Nb, mi, mo, wc, Ki, Ko, Kbh, Cbh, c, D=0, L=0, phi_D0=0):
+def ode_system(t, y, Fr, Nb, mi, mo, wc, Ki, Ko, Kbh, Cbh, c, dC=0, phi_Do=0, phi_oc=0):
     xi, yi, xo, yo, dxi, dyi, dxo, dyo = y
     phi_0 = wc * t
     phi = phi_0 + np.arange(Nb) / Nb * 2 * pi
-    delta = delta_calculate(xi, yi, xo, yo, phi, c)
+    phi = np.mod(phi, 2 * pi)
+    # 外滚道故障位移激励控制
+    Hdo = np.zeros_like(phi)
+    if dC != 0 and phi_Do != 0:
+        Hdo = Hdo_calculate(phi, dC, phi_Do, phi_oc)
+    # 非线性(故障)激励计算
+    delta = delta_calculate(xi, yi, xo, yo, phi, c, Hdo)
     fx, fy = contact_force(phi, delta, Ki, Ko)
     ddxi = (-fx - xi) / mi
     ddyi = (Fr - fy - yi) / mi
@@ -21,7 +27,33 @@ def contact_force(phi, delta, Ki, Ko):
     return fx, fy
 
 
-def delta_calculate(xi, yi, xo, yo, phi, c):
-    delta = (xi - xo) * cos(phi) + (yi - yo) * sin(phi) - c
+def delta_calculate(xi, yi, xo, yo, phi, c, Hdo):
+    delta = (xi - xo) * cos(phi) + (yi - yo) * sin(phi) - c - Hdo
     delta = np.maximum(delta, 0)
+    # delta_0 = (xi - xo) * cos(phi) + (yi - yo) * sin(phi) - c
+    # delta_0 = np.maximum(delta_0, 0)
+    # print(delta_0,'\n',Hdo,'\n')
     return delta
+
+
+def Hdo_calculate(phi, dC, phi_do, phi_oc=0):
+    # 向量操作优化
+    Hdo = np.zeros_like(phi)
+    mask = (np.abs(phi - phi_oc) <= phi_do)
+    Hdo[mask] = dC * np.cos((phi[mask] - phi_oc) * np.pi / (2 * phi_do))
+    return Hdo
+
+# 原版
+# def Hdo_calculate(dC, phi, phi_do, phi_oc):
+#     phi = np.atleast_1d(phi)
+#     Hdo = np.zeros_like(phi)
+#     for i in range(len(phi)):
+#         if phi[i] < phi_oc and phi[i] >= phi_oc - phi_do:
+#             Hdo[i] = dC * cos((phi[i] - phi_oc) * np.pi / 2 / phi_do)
+#         elif phi[i] == phi_oc:
+#             Hdo[i] = dC
+#         elif phi[i] >= phi_oc and phi[i] <= phi_oc + phi_do:
+#             Hdo[i] = dC * cos((phi[i] - phi_oc) * np.pi / 2 / phi_do)
+#         else:
+#             Hdo[i] = 0
+#     return Hdo
--- main.py
+++ main.py
@@ -74,4 +74,68 @@ def healthy_bearing(savedata=False):
 
 if __name__ == '__main__':
     matplotlib.use('TkAgg')
-    healthy_bearing(savedata=True)
+    # healthy_bearing(savedata=True)
+
+    filename = 'NUP205'
+    Ki, Ko = load_K(filename)
+    Nb = 13
+    mi = 5324.83e-9 * 7850
+    mo = 7664.64e-9 * 7850
+    Kbh = 1e7
+    Cbh = 1e3
+    Fr = 500
+    Rm = 19.5e-3
+    Rr = 3.75e-3
+    c = 1e-6
+    wi = 3000 / 60 * 2 * np.pi
+    wc = wi / 2 * (1 - Rr / Rm)
+    fs = int(1e5)
+    T = 1
+    t_span = np.linspace(0, T, T * fs + 1)
+    DOF = 4
+    y0 = np.zeros(DOF * 2)
+    '''
+    基于动力学的滚动轴承多点故障建模及振动特性研究_李君飞.pdf --> page27
+    相较于健康工况，增加参数dC ，phi_do, phi_oc
+    '''
+    B = 0.2e-3  # 故障宽度 mm
+    Cd = Rr - np.sqrt(Rr ** 2 - (B / 2) ** 2)
+    Ro = Rm + Rr / 2
+    Co = Ro - np.sqrt(Ro ** 2 - (B / 2) ** 2)
+    dC = Cd - Co
+    phi_do = np.arcsin(B / 2 / Ro)
+    phi_oc = np.pi
+    # 角度-外滚道故障位移激励测试
+    phi = np.linspace(0, 2 * np.pi, 1000)
+    Hdo = BearingModel.Hdo_calculate(phi, dC, phi_do, phi_oc)
+    plt.figure(1)
+    plt.plot(phi, Hdo)
+    plt.savefig(f"{filename}/{filename}_Hdo.png", dpi=300)
+    plt.close()
+    # def ode_system(t, y, Fr, Nb, mi, mo, wc, Ki, Ko, Kbh, Cbh, c, dC=0, phi_Do=0, phi_oc=0)
+    result = solve_ivp(BearingModel.ode_system, [0, T], y0, t_eval=t_span,
+                       args=(Fr, Nb, mi, mo, wc, Ki, Ko, Kbh, Cbh, c, dC, phi_do, phi_oc),
+                       max_step=1e-5, rtol=1, atol=1)
+    y = result.y
+    signal = y[7, :]
+    signal = np.diff(signal) * fs
+    t = np.linspace(0, T, len(signal))
+    plt.figure(1)
+    plt.plot(t, signal)
+    plt.ylim([-3, 3])
+    plt.xlim([0.948, 0.964])
+    plt.savefig(f"{filename}/{filename}_t_outer.png", dpi=300)
+    plt.close()
+    plt.figure(2)
+    y_f, y_a = fft_trans(signal[int(0.2 * fs):], fs)
+    plt.plot(y_f, y_a)
+    plt.xlim([0, 1000])
+    plt.ylim([0, 0.0011])
+    plt.savefig(f"{filename}/{filename}_f_outer.png", dpi=300)
+    plt.close()
+    if True:
+        data = np.zeros((4, fs * T))
+        for i in range(4):
+            data[i] = np.diff(y[4 + i, :]) * fs
+        data = pandas.DataFrame(data)
+        data.to_csv(f"{filename}/{filename}_outer.csv", header=False, index=False)
